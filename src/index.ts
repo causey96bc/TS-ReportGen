656616ffa7c0d37ebdac0de4c8add49d980d8980
brettc <causey904.bc@gmail.com>
2021-01-10 13:42:04 -0500

    parsing intital data

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,18 @@
-console.log("hello world");
-let hello = "hello"
-console.log(hello);
+import { CsvFileReader } from "./CsvFileReader";
+
+const reader = new CsvFileReader('football.csv');
+reader.read();
+let manUnitedWins = 0;
+      enum MatchResult {
+          HomeWin = 'H',
+          AwayWin = 'A',
+          Draw = 'D'
+      };
+      for (let match of reader.data) {
+        if (match[1] === "Man United" && match[5] === MatchResult.HomeWin) {
+          manUnitedWins++;
+        } else if (match[2] === "Man United" && match[5] === MatchResult.AwayWin) {
+          manUnitedWins++;
+        }
+      }
+console.log(`man united won ${manUnitedWins} games`);
