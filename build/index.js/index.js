024d4c760772af6dbcc469429d63c129d73d6e7e
brettc <causey904.bc@gmail.com>
2021-01-14 22:22:10 -0500

    adding wins analysis

--- build/index.js/index.js
+++ build/index.js/index.js
@@ -1,21 +1,9 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 var MatchReader_1 = require("./MatchReader");
-var MatchResult_1 = require("./MatchResult");
 var CsvFileReader_1 = require("./CsvFileReader");
 var csvFileReader = new CsvFileReader_1.CsvFileReader('football.csv');
 var matchReader = new MatchReader_1.MatchReader(csvFileReader);
 matchReader.load();
 // const dateOfFirstMatch = reader.data[0][0];
 // console.log(dateOfFirstMatch);
-var manUnitedWins = 0;
-for (var _i = 0, _a = matchReader.matches; _i < _a.length; _i++) {
-    var match = _a[_i];
-    if (match[1] === "Man United" && match[5] === MatchResult_1.MatchResult.HomeWin) {
-        manUnitedWins++;
-    }
-    else if (match[2] === "Man United" && match[5] === MatchResult_1.MatchResult.AwayWin) {
-        manUnitedWins++;
-    }
-}
-console.log("man united won " + manUnitedWins + " games");
--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,8 @@
 import { MatchReader } from "./MatchReader";
-import { MatchResult } from "./MatchResult";
 import { CsvFileReader } from "./CsvFileReader";
 const csvFileReader = new CsvFileReader('football.csv');
 const matchReader = new MatchReader(csvFileReader);
 matchReader.load();
 // const dateOfFirstMatch = reader.data[0][0];
 // console.log(dateOfFirstMatch);
-let manUnitedWins = 0;
-      for (let match of matchReader.matches) {
-        if (match[1] === "Man United" && match[5] === MatchResult.HomeWin) {
-          manUnitedWins++;
-        } else if (match[2] === "Man United" && match[5] === MatchResult.AwayWin) {
-          manUnitedWins++;
-        }
-      }
-console.log(`man united won ${manUnitedWins} games`);
+
